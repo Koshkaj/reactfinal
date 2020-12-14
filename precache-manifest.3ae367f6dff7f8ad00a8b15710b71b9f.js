28c7bfa425a61f65fccdf4250c3bb2f0aa223561
davitk <dkoshkeli@gmail.com>
2020-12-14 14:19:18 +0400

    Updates

---
+++ precache-manifest.3ae367f6dff7f8ad00a8b15710b71b9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9ac051ada03626ad407279b5b2c4007",
+    "url": "/reactfinal/index.html"
+  },
+  {
+    "revision": "e90518a8fb3c3228e168",
+    "url": "/reactfinal/static/css/main.8fe13049.chunk.css"
+  },
+  {
+    "revision": "4b7a02b33e3f69fa40a7",
+    "url": "/reactfinal/static/js/2.e9837d6c.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/reactfinal/static/js/2.e9837d6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e90518a8fb3c3228e168",
+    "url": "/reactfinal/static/js/main.6d27d2eb.chunk.js"
+  },
+  {
+    "revision": "2fb1b608bc064c6e1b25",
+    "url": "/reactfinal/static/js/runtime-main.8c2e13c8.js"
+  }
+]);
